d18e7bf5e829e6a1373fc748c14ea6b172bb8b60
kernel <fangwaine@outlook.com>
2022-08-31 18:26:12 +0800

    jdk动态代理

--- spring-aop/src/main/java/com/wayne/aop/proxy/JdkProxy.java
+++ spring-aop/src/main/java/com/wayne/aop/proxy/JdkProxy.java
@@ -16,9 +16,11 @@ public class JdkProxy {
         final ClassLoader classLoader = userDaoImpl.getClass().getClassLoader();
         final Class<?>[] interfaces = userDaoImpl.getClass().getInterfaces();
         UserDaoProxy userDaoProxy = new UserDaoProxy(userDaoImpl);
+        // 使用jdk动态代理创建代理对象，需要三个参数：类加载器、被代理类实现的接口、实现InvocationHandler接口的一个实例
         final UserDao userDao = (UserDao)Proxy.newProxyInstance(classLoader, interfaces, userDaoProxy);
         Object obj = userDao.add(1,2);
-        log.info("{}", obj);
+        final String update = userDao.update("1");
+        log.info("add: {}, update: {}", obj, update);
     }
 }
 
